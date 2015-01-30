60e63c3e2e7ae83e8677ea6ea8aee67c77ff99f5
Baguz A <eggoez@users.noreply.github.com>
2015-01-30 14:21:03 +0700

    Create theme.conf.php

---
+++ theme.conf.php
@@ -0,0 +1,28 @@
+<?php
+	$theme['name'] = 'TmeaSarap';
+	$theme['author'] = 'goez';
+	$theme['www'] = 'http://eggoez.com/flatpress';
+	$theme['description'] = 'Simple Themes for blog FlatPress use BootStrap'.
+							'proyek';
+	$theme['version'] = 1.0;
+	$theme['style_def'] = 'style.css';
+	$theme['style_admin'] = 'admin.css';
+	remove_filter('admin_head', 'admin_head_action'); 
+
+		// adds a javascript (expand/collapse) thanks for old fp themes flatmaas2
+	function theme_flatmaas2_head() {
+
+		global $blog_config;
+		
+		echo "<!-- Theme Head -->\n";
+		echo '<script src="' . THEMES_DIR . 
+			$blog_config['THEME'] .
+			'TemaSarap/res/toggleMenu.js" type="text/javascript"></script>';
+		echo "\n<!-- EndOf Theme Head -->\n";
+	}
+	
+	add_action('wp_head', 'theme_TemaSarap_head');
+	register_widgetset('right');
+	register_widgetset('left'); 
+	register_widgetset('menubar');
+?>
