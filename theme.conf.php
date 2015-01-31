e46210d482c5594bab66b82018a0a04900345004
Baguz A <eggoez@users.noreply.github.com>
2015-01-31 14:54:31 +0700

    Update theme.conf.php

--- theme.conf.php
+++ theme.conf.php
@@ -1,36 +1,42 @@
 <?php
-/*  
+/*
 Theme Name: TemaSarap
 Theme URI: http://eggoez.com/flatpress
 Description: Just created themes from bootstrap
-Version: 1.0
+Version: 0.702;
 Author: eggoez
 Author URI: http://goez.my.id
 */
 	$theme['name'] = 'botsarap';
-	$theme['author'] = 'eggoez';
+	$theme['author'] = 'goez';
 	$theme['www'] = 'http://goez.my.id';
 	$theme['description'] = 'theme bot'.
 							'sarap';
 	
-	$theme['version'] = 1.0.18;
+	$theme['version'] = 0.702;
 		
 	$theme['style_def'] = 'style.css';
 	$theme['style_admin'] = 'admin.css';
+	
+	// Other theme settings
+	
+		// overrides default tabmenu
+		// and panel layout
 	remove_filter('admin_head', 'admin_head_action'); 
-	function theme_ts_head() {
+
+		// adds a javascript (expand/collapse)
+	function theme_bs_head() {
 		
 		global $blog_config;
 		
 		echo "<!-- Theme Head -->\n";
 		echo '<script src="' . THEMES_DIR . 
 			$blog_config['THEME'] .
-			'bs/res/bootstrap.min.js" type="text/javascript"></script>';
+			'bs/res/botstrap.min.js" type="text/javascript"></script>';
 		echo "\n<!-- EndOf Theme Head -->\n";
 	}
 	
 	add_action('wp_head', 'theme_bs_head');
-	register_widgetset('top');
 	register_widgetset('right');
 	register_widgetset('left'); 
 	register_widgetset('menubar');
